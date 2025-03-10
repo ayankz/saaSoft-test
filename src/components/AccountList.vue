5be0248cef5bad743d3aec5fb170c2bc1660fe88
ayankz <ayanpost87@gmail.com>
2025-03-11 02:33:08 +0500

    Скрывать password если select LDAP

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -8,12 +8,12 @@
   </div>
   <div class="flex items-center gap-4 p-2 w-full" v-for="account in accounts" :key="account.id">
     <input @blur="validate($event, account)" type="text" :value="account.labels.map((label) => label.text).join('; ')" class="border border-gray-300 rounded p-2 min-w-0" />
-    <select class="border border-gray-300 rounded p-2 min-w-0" name="type" :value="account.type">
+    <select class="border border-gray-300 rounded p-2 min-w-0" name="type" v-model="account.type">
       <option value="LDAP">LDAP</option>
       <option value="Локальная">Локальная</option>
     </select>
     <input type="text" :class="{ 'flex-1': account.password, 'flex-[2]': !account.password }" :value="account.login" class="border border-gray-300 rounded p-2 min-w-0" />
-    <div v-if="account.password" class="relative flex-1 min-w-0">
+    <div v-if="account.type !== 'LDAP'" class="relative flex-1 min-w-0">
       <input :type="account.isPasswordVisible ? 'text' : 'password'" :value="account.password" class="w-full border border-gray-300 rounded p-2 pr-10" />
       <button @click="togglePassword(account)" type="button" class="absolute inset-y-0 right-2 flex items-center px-2 text-gray-500">
         <OpenEyeIcon v-if="account.isPasswordVisible" class="w-5 h-5 cursor-pointer" />
@@ -31,11 +31,11 @@
   import CloseEye from "../components/icons/CloseEye.vue";
   import { storeToRefs } from "pinia";
   import { useAccountsStore, type Account } from "@/stores/accounts";
+  import { watch } from "vue";
 
   const accountsStore = useAccountsStore();
   const { accounts } = storeToRefs(accountsStore);
   const togglePassword = (account: Account) => {
-    console.log(account);
     account.isPasswordVisible = !account.isPasswordVisible;
   };
   const validate = (event: Event, account: Account) => {
