061b7c1828fe0d3a8b58a94565e2ccd21ba36a44
ayankz <ayanpost87@gmail.com>
2025-03-10 20:51:12 +0500

    Добавил основные компоненты, tailwind, ci/cd yaml file

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container flex flex-col gap-6 mx-auto p-4">
+    <div class="flex gap-2 items-center">
+      <div class="text-2xl font-bold">Мои проекты</div>
+      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border cursor-pointer border-blue-500 hover:border-transparent rounded">+</button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <InfoBlock />
+    <div class="grid grid-cols-5 gap-4 p-2 text-gray-500">
+      <span>Метки</span>
+      <span>Тип записи</span>
+      <span>Логин</span>
+      <span>Пароль</span>
+      <span></span>
+    </div>
+    <div class="grid grid-cols-5 gap-4 p-2">
+      <input type="text" class="border border-gray-300 rounded p-2" />
+      <select class="border border-gray-300 rounded p-2" name="type" id="">
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+      <input type="text" class="border border-gray-300 rounded p-2" />
+      <input type="password" class="border border-gray-300 rounded p-2" />
+      <BasketIcon />
+    </div>
+    <div class="grid grid-cols-5 gap-4 p-2">
+      <input type="text" class="border border-gray-300 rounded p-2" />
+      <select class="border border-gray-300 rounded p-2" name="type" id="">
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+      <input type="text" class="border border-gray-300 rounded p-2" />
+      <input type="password" class="border border-gray-300 rounded p-2" />
+      <BasketIcon />
+    </div>
+    <div class="grid grid-cols-5 gap-4 p-2">
+      <input type="text" class="border border-gray-300 rounded p-2" />
+      <select class="border border-gray-300 rounded p-2" name="type" id="">
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+      <input type="text" class="border border-gray-300 rounded p-2 col-span-2" />
+      <BasketIcon />
+    </div>
+    <div class="grid grid-cols-5 gap-4 p-2">
+      <input type="text" class="border border-gray-300 rounded p-2" />
+      <select class="border border-gray-300 rounded p-2" name="type" id="">
+        <option value="1">1</option>
+        <option value="2">2</option>
+      </select>
+      <input type="text" class="border border-gray-300 rounded p-2 col-span-2" />
+      <BasketIcon />
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script setup>
+  import BasketIcon from "@/components/icons/BaksetIcon.vue";
+  import InfoBlock from "@/components/InfoBlock.vue";
+</script>
---
+++ src/components/InfoBlock.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex items-center gap-6 bg-blue-500 text-white p-2 rounded">
+    <QuestionIcon />
+    <span class="text-xs">Для указания нескольких меток для одной пары логин/пароль используйте разделитель;</span>
+  </div>
+</template>
+<script setup>
+  import QuestionIcon from "@/components/icons/QuestionIcon.vue";
+</script>
